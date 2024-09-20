eb747fd1ca34ebfebff66e60cf99a5a6717b9b85
Ashish Jaiswal <ashish@obmondo.com>
2024-09-20 09:40:22 +0530

    bug fix: exec will not create file, if the file is already exists, so lets not create one

--- modules/enableit/profile/manifests/certs/manual.pp
+++ modules/enableit/profile/manifests/certs/manual.pp
@@ -1,4 +1,7 @@
 # Manual certificate
+# TODO: lets not accept expired cert from users.
+# need to update the underlying module
+# openssl::cert_date_valid($_cert_file)
 define profile::certs::manual (
   String               $key,
   String               $cert,
@@ -9,45 +12,43 @@ define profile::certs::manual (
   Optional[String]     $ca     = undef,
 ) {
 
-  File {
-    noop => false,
-  }
-
   $_parts_dir     = "${base_dir_parts}/${name}"
   $_cert_file     = "${_parts_dir}/cert.pem"
   $_cert_key      = "${_parts_dir}/key.pem"
   $_cert_ca       = "${_parts_dir}/ca.pem"
   $_cert_combined = "${base_dir_combined}/${name}.pem"
 
-  # TODO: lets not accept expired cert from users.
-  # need to update the underlying module
-  # openssl::cert_date_valid($_cert_file)
+  File {
+    ensure => 'file',
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0400',
+    noop   => false,
+  }
 
   file { $_parts_dir:
     ensure => 'directory',
   }
 
-  File {
-    ensure  => 'file',
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0400',
-    require => File[$_parts_dir],
-    notify  => Exec["write combined cert ${name}"],
-  }
-
-  file { $_cert_file:
-    content => "${cert}\n",
-    mode    => '0600',
-  }
-
-  file { $_cert_key:
-    content => "${key}\n",
+  file {
+    default:
+      require => File[$_parts_dir],
+      notify  => Exec["write combined cert ${name}"],
+    ;
+    $_cert_file:
+      content => "${cert}\n",
+      mode    => '0600',
+    ;
+    $_cert_key:
+      content => "${key}\n",
+    ;
   }
 
   if $ca {
     file { $_cert_ca:
       content => "${ca}\n",
+      require => File[$_parts_dir],
+      notify  => Exec["write combined cert ${name}"],
     }
   }
 
@@ -64,16 +65,7 @@ define profile::certs::manual (
     path        => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
     refreshonly => true,
     creates     => $_cert_combined,
-    subscribe   => File[
-      $_cert_file,
-      $_cert_key,
-      if $ca { $_cert_ca },
-    ],
     require     => File[$base_dir_combined],
-  }
-
-  file { $_cert_combined:
-    ensure => 'file',
   }
 
   monitor::domains::expiry { $domain:
