afed635aea36ca3a51e33568335802474f21103f
Sanskar Bhushan <sanskar@obmondo.com>
2024-11-11 10:57:48 +0530

    adding packages facts to suse
    Signed-off-by: Sanskar Bhushan <sanskar@obmondo.com>

--- modules/enableit/functions/lib/facter/packages.rb
+++ modules/enableit/functions/lib/facter/packages.rb
@@ -4,7 +4,7 @@ rpm_package_re = /^(?<name>.+?)-(?<version>\d.*?)\.(?<distro>[a-z0-9]+?)\.(?<arc
 
 Facter.add(:packages) do
   confine :kernel => :linux
-  confine :osfamily => :redhat
+  confine :osfamily => [:redhat, :suse]
 
   setcode do
     packages = Facter::Util::Resolution.exec('rpm -qa')
