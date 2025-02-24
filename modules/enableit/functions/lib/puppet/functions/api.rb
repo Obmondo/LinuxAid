d23e80e2e07fbf04f02f86e97e4a94bad130a9dc
Rishikesh <rishikesh@obmondo.com>
2025-02-24 15:31:36 +0530

    added ruby function to use puppet certs to communicate with api

--- modules/enableit/functions/lib/puppet/functions/api.rb
+++ modules/enableit/functions/lib/puppet/functions/api.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
-require 'json'
-require 'net/http'
+require "json"
+require "net/http"
+require "openssl"
 
 SSL = true
 PORT = nil
@@ -9,25 +10,49 @@ PORT = nil
 class HTTPNotFound < ArgumentError
 end
 
-def obmondo_api(endpoint)
-  raise NameError, "invalid endpoint '#{endpoint}'" unless
-    endpoint.is_a? String
+def obmondo_api(endpoint, method = "GET", use_basic_auth = true)
+  raise NameError, "invalid endpoint '#{endpoint}'" unless endpoint.is_a? String
+  raise ArgumentError, "invalid method '#{method}'" unless %w[GET PUT].include?(method.upcase)
 
-  proto = "http#{'s' if SSL}"
-  port = if PORT
-           ":#{PORT}"
-         end
+  proto = "http#{"s" if SSL}"
+  port = (":#{PORT}" if PORT)
   uri = URI("#{proto}://api.obmondo.com#{port}/api#{endpoint}")
 
+  # Direct paths for the certificate and private key
+  cert_path = "/etc/puppetlabs/puppet/ssl/certs/puppet.pem"
+  key_path = "/etc/puppetlabs/puppet/ssl/private_keys/puppet.pem"
+
+  # Load the certificate and private key from the specified files
+  cert = OpenSSL::X509::Certificate.new(File.read(cert_path))
+  key = OpenSSL::PKey::RSA.new(File.read(key_path))
+
   begin
-    Net::HTTP.start(uri.host, uri.port, use_ssl: SSL) do |http|
-      response = http.get(uri.path,
-                          'Authorization' => 'Basic cmVhZG9ubHk6cmVhZG9ubHk=')
+    Net::HTTP.start(uri.host, uri.port, use_ssl: SSL, cert: cert, key: key) do |http|
+      request = case method.upcase
+      when "GET"
+        Net::HTTP::Get.new(uri)
+      when "PUT"
+        Net::HTTP::Put.new(uri)
+      else
+        raise ArgumentError, "Unsupported HTTP method: #{method}"
+      end
 
-      raise HTTPNotFound, response if response.code.to_i == 404
-      return JSON.parse(response.body) if response.code.to_i == 200
+      if use_basic_auth
+        request["Authorization"] = "Basic cmVhZG9ubHk6cmVhZG9ubHk=" # Base64-encoded username:password
+      end
 
-      raise ArgumentError, "request failed: #{response}"
+      response = http.request(request)
+
+      case response.code.to_i
+      when 200
+        return JSON.parse(response.body)
+      when 204
+        break
+      when 404
+        raise HTTPNotFound, response
+      else
+        raise ArgumentError, "request failed: #{response}"
+      end
     end
   rescue SocketError => e
     raise "API: unable to contact obmondo API #{e}"
@@ -38,8 +63,8 @@ end
 
 def split_certname(certname)
   match = certname
-          .match(/^(?<node_name>[a-z0-9-]+)\.(?<customer_id>[a-z0-9-]+$)/)
-  raise ArgumentError, 'invalid certname' unless match
+    .match(/^(?<node_name>[a-z0-9-]+)\.(?<customer_id>[a-z0-9-]+$)/)
+  raise ArgumentError, "invalid certname" unless match
 
   match
 end
--- puppet_autosign.rb
+++ puppet_autosign.rb
@@ -1,10 +1,10 @@
 #!/opt/puppetlabs/puppet/bin/ruby
 # https://puppet.com/docs/puppet/latest/ssl_autosign.html#policy-based-autosigning
 
-require_relative './modules/enableit/functions/lib/puppet/functions/api.rb'
+require_relative "modules/enableit/functions/lib/puppet/functions/api"
 
-CERTNAME=ARGV[0]
+CERTNAME = ARGV[0]
 
-Kernel.exit(0) if CERTNAME == 'puppetdb' || !obmondo_api("/server/#{CERTNAME}").nil?
+Kernel.exit(0) if CERTNAME == "puppetdb" || !obmondo_api("/servers/puppet/certname/#{CERTNAME}", "PUT", false).nil?
 
 Kernel.exit(1)
