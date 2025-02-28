21db7dad82b77708e8acdd6ed320200cadbfb634
Deepak Tiwari <deepak@obmondo.com>
2025-02-28 17:51:56 +0530

    Updating the smtpd_recipient_restrictions config in postfix

--- modules/enableit/kolab/manifests/postfix.pp
+++ modules/enableit/kolab/manifests/postfix.pp
@@ -203,7 +203,11 @@ class kolab::postfix (
     smtpd_recipient_restrictions => [
       'permit_mynetworks',
       'reject_unauth_pipelining',
-      'reject_rbl_client zen.spamhaus.org',
+      'reject_rbl_client zen.spamhaus.org=127.0.0.[2..11]',
+      'reject_rhsbl_sender dbl.spamhaus.org=127.0.1.[2..99]',
+      'reject_rhsbl_helo dbl.spamhaus.org=127.0.1.[2..99]',
+      'reject_rhsbl_reverse_client dbl.spamhaus.org=127.0.1.[2..99]',
+      'warn_if_reject reject_rbl_client zen.spamhaus.org=127.255.255.[1..255]',
       'reject_non_fqdn_recipient',
       'reject_invalid_helo_hostname',
       'reject_unknown_recipient_domain',
