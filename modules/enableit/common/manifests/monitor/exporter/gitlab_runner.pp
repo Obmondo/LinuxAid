cbf90511b6e252973673421b10aec5c3dc7d0337
Deepak Tiwari <deepak@obmondo.com>
2025-06-24 15:03:38 +0530

    feat: enable Prometheus metrics for GitLab Runner

---
+++ modules/enableit/common/manifests/monitor/exporter/gitlab_runner.pp
@@ -0,0 +1,20 @@
+# Prometheus gitlab_runner exporter
+class common::monitor::exporter::gitlab_runner (
+  Boolean             $enable         = $common::monitr::exporter::enable,
+  Boolean[false]      $noop_value     = false,
+  Eit_types::IPPort   $listen_address = '127.254.254.254:63384',
+  Eit_types::Certname $host           = $trusted['certname'],
+) {
+
+  File {
+    noop => $noop_value
+  }
+
+  prometheus::scrape_job { 'gitlab_runner' :
+    job_name    => 'gitlab_runner',
+    tag         => $::trusted['certname'],
+    targets     => [ "${listen_address}" ],
+    labels      => { 'certname' => $::trusted['certname'] },
+    collect_dir => '/etc/prometheus/file_sd_config.d',
+  }
+}
