f3cbfb6f23a76541706d54c66b24a6d2099a900b
Ashish Jaiswal <ashish@obmondo.com>
2025-06-25 02:30:46 +0200

    fix: typo missing comma at the end for runner type

--- modules/enableit/eit_types/types/gitlab/runner/config.pp
+++ modules/enableit/eit_types/types/gitlab/runner/config.pp
@@ -1,7 +1,7 @@
 type Eit_types::Gitlab::Runner::Config = Struct[{
   'url'                => Stdlib::HTTPSUrl,
   'registration-token' => String,
-  'executor'           => Enum['docker', 'shell']
+  'executor'           => Enum['docker', 'shell'],
   'image'              => Optional[Eit_types::DockerImage],
   'tlsverify'          => Optional[Boolean],
   'privileged'         => Optional[Boolean],
