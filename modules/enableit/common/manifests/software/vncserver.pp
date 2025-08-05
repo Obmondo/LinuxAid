df07f5b33ad37d79098f6ab08bc6db936bc04e3e
Deepak Tiwari <deepak@obmondo.com>
2025-08-05 20:31:22 +0530

    Add support for ubuntu session on vnc class

--- modules/enableit/common/manifests/software/vncserver.pp
+++ modules/enableit/common/manifests/software/vncserver.pp
@@ -14,11 +14,12 @@ class common::software::vncserver (
   Boolean                                  $manage           = false,
   Boolean                                  $enable           = false,
   Struct[{
-      session  => Enum['gnome', 'kde', 'xfce', 'lxde'],
+      session  => Enum['gnome', 'kde', 'xfce', 'lxde', 'ubuntu'],
       geometry => Enum['2000x1200', '1280x1024', '1920x1080', '1920x1200'],
   }]                                       $config_defaults  = {},
   Hash[String, Stdlib::Port]               $vnc_users        = {},
   Optional[Boolean]                        $noop_value       = false,
+  Enum['vncserver', 'tigervncserver']      $systemd_service  = 'vncserver',
 ) inherits common {
   if $manage {
     contain profile::software::vncserver
