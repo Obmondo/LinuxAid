34189a25fe59cc46dec739b835d3d5cc75a4284d
mdasif <mdasif@obmondo.com>
2025-08-20 19:36:04 +0530

    Add prune duration variable for Docker system prune job https://gitea.obmondo.com/EnableIT/LinuxAid/issues/1102

--- modules/enableit/profile/manifests/virtualization/docker.pp
+++ modules/enableit/profile/manifests/virtualization/docker.pp
@@ -17,6 +17,7 @@ class profile::virtualization::docker (
   Boolean                              $prune_volume        = $role::virtualization::docker::prune_volume,
   Hash[Eit_types::Domain, Hash]        $registry            = $role::virtualization::docker::registry,
   Boolean                              $upstream_repo       = $role::virtualization::docker::upstream_repo,
+  Optional[Eit_types::SimpleString]    $prune_duration      = $role::virtualization::docker::prune_duration,
 ) {
 
   $__cadvisor_listen_port = 63392
@@ -209,7 +210,7 @@ class profile::virtualization::docker (
 
   profile::cron::job { 'regularly prune docker system':
     enable  => $prune_system,
-    command => 'chronic docker system prune --all --force --filter until=$(( 30 * 24 ))h',
+    command => "chronic docker system prune --all --force --filter until=$(( ${prune_duration} * 24 ))h",
     user    => 'root',
     hour    => 2,
     minute  => 5,
