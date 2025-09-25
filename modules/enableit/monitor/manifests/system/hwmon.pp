f580a2433b4bb60e0ca79bf319091b51a35db269
Sidharth Jawale <sidharth@obmondo.com>
2025-09-25 15:56:39 +0530

    chore: replace the sudoers manage logic without using lookup, by directly setting in common
    Signed-off-by: Sidharth Jawale <sidharth@obmondo.com>

--- modules/enableit/monitor/manifests/system/hwmon.pp
+++ modules/enableit/monitor/manifests/system/hwmon.pp
@@ -5,13 +5,13 @@
 class monitor::system::hwmon (
   Boolean $enable = true,
 ) {
-  if dig($facts, 'dmi', 'board', 'product') == 'PRIME X670-P WIFI' {
+  if $facts['dmi']['board']['product'] == 'PRIME X670-P WIFI' {
     file { '/etc/modprobe.d/nct6775.conf':
-      ensure  => 'file',
+      ensure  => 'present',
       content => 'options nct6775 force_id=0xd420',
     }
     file { '/etc/modules-load.d/nct6775.conf':
-      ensure  => 'file',
+      ensure  => 'present',
       content => 'nct6775',
     }
   }
