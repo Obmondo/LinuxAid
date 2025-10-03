18a24352a0e4b0ee2f7956edb6f6af33f166419a
Sidharth Jawale <sidharth@obmondo.com>
2025-10-03 17:27:10 +0530

    fix: rectify the existing facters to properly check and execute
    Signed-off-by: Sidharth Jawale <sidharth@obmondo.com>

--- modules/enableit/functions/lib/facter/iptable_rules_exist.rb
+++ modules/enableit/functions/lib/facter/iptable_rules_exist.rb
@@ -1,14 +1,14 @@
 # iptable_rules_exist returns a bool value based on the presence of iptable rules
 # on the host system 
 Facter.add('iptable_rules_exist') do
-  setcode do
-
-    # Only proceed further if the following conditions are met
-    confine kernel: :linux
-    confine { Facter::Core::Execution.which('iptables-save') }
-
-    # Execute the iptables-save command and check if there's an output
-    !Facter::Core::Execution.execute('iptables-save -c').strip.empty?
-
+  confine kernel: 'Linux'
+  setcode do  
+    # Only proceed if 'iptables-save' command is available  
+    if Facter::Core::Execution.which('iptables-save')
+      # Execute the iptables-save command and check if there's an output
+      !Facter::Core::Execution.execute('iptables-save -c').strip.empty?
+    else
+      nil
+    end
   end
 end
