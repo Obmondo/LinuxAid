f0ed26fa7349142d8dbc0ad69f02fad68c87de9c
Deepak Tiwari <deepak@obmondo.com>
2025-10-07 13:07:34 +0530

    Manage PyCharm and miniforge installation from Linuxaid

---
+++ modules/enableit/common/manifests/software/miniforge.pp
@@ -0,0 +1,21 @@
+# @summary Class for managing the Miniforge software
+#
+# @param manage Boolean parameter to control management of Miniforge. Defaults to false.
+#
+# @param enable Boolean parameter to enable or disable Miniforge. Defaults to true.
+#
+# @param version String parameter to control version of Miniforge3.
+#
+# @param noop_value Optional boolean to specify noop mode value.
+#
+class common::software::miniforge (
+  Boolean            $manage     = true,
+  Boolean            $enable     = true,
+  Eit_types::Version $version    = '25.3.1-0',
+  Optional[Boolean]  $noop_value = false,
+) {
+
+  if $manage {
+    contain profile::software::miniforge
+  }
+}
