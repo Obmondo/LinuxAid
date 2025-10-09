0d03131d7702d344d1220fd8b16cb6d9a43791a3
Ashish Jaiswal <ashish@obmondo.com>
2025-10-09 10:26:54 +0530

    chore: deleted rvm module, since its not used anymore

--- modules/upstream/rvm/lib/facter/rvm_installed.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-Facter.add(:rvm_installed) do
-  rvm_binary = '/usr/local/rvm/bin/rvm'
-
-  setcode do
-    File.exist? rvm_binary
-  end
-end
--- modules/upstream/rvm/lib/facter/rvm_version.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-Facter.add(:rvm_version) do
-  rvm_binary = '/usr/local/rvm/bin/rvm'
-
-  setcode do
-    File.exist?(rvm_binary) ? `#{rvm_binary} version`.strip.match(%r{rvm ([0-9]+\.[0-9]+\.[0-9]+)(?:-next)? .*})[1] : nil
-  end
-end
