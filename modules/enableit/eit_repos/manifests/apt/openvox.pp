16b734ec399e36d75b3efcf3b4194948b8ce2ab3
Deepak Tiwari <deepak@obmondo.com>
2025-10-10 12:49:10 +0530

    feat: Enable OpenVox repository for yum distribution after agent migration to OpenVox

--- modules/enableit/eit_repos/manifests/apt/openvox.pp
+++ modules/enableit/eit_repos/manifests/apt/openvox.pp
@@ -4,7 +4,7 @@ class eit_repos::apt::openvox (
   Optional[Boolean] $noop_value = $eit_repos::noop_value,
 ) {
 
-    # We most likely don't want $noop_value to be `true` (because that causes noop
+  # We most likely don't want $noop_value to be `true` (because that causes noop
   # to be forced); we most likely intend to use `undef` instead.
   if $noop_value {
     notify { '$noop_value is true!': }
