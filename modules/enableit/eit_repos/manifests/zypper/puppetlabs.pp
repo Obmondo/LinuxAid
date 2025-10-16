1df37282fd6f4196e42570176429b89d155d7bff
Ashish Jaiswal <ashish@obmondo.com>
2025-10-16 14:02:15 +0530

    fix: removed puppetlabs repo from the node for yum and suse OS

--- modules/enableit/eit_repos/manifests/zypper/puppetlabs.pp
+++ modules/enableit/eit_repos/manifests/zypper/puppetlabs.pp
@@ -1,16 +1,10 @@
 # PuppetLabs
 class eit_repos::zypper::puppetlabs (
-  Boolean           $ensure     = true,
+  Boolean           $ensure     = false,
   Optional[Boolean] $noop_value = $eit_repos::noop_value,
 
   Enum['http', 'https'] $source_protocol = lookup('eit_repos::source_protocol'),
 ) {
-
-  # Remove the repo if puppet agent version is not 7
-  $_ensure = $facts['puppetversion'] ? {
-    /^7.*/ => $ensure,
-    default => false
-  }
 
   # We most likely don't want $noop_value to be `true` (because that causes noop
   # to be forced); we most likely intend to use `undef` instead.
@@ -21,18 +15,20 @@ class eit_repos::zypper::puppetlabs (
   $suseversion = $facts['os']['release']['major']
 
   # Obmondo Puppetlabs Repo
-  zypprepo { "obmondo_puppetlabs7":
-    ensure      => ensure_present($_ensure),
-    name        => "obmondo_puppetlabs7",
-    baseurl     => "${source_protocol}://repos.obmondo.com/puppetlabs/sles/puppet7/${suseversion}/\$basearch/",
-    enabled     => 1,
-    autorefresh => 1,
-    gpgcheck    => 1,
-    type        => 'rpm-md',
+  [7, 8].each |$version| {
+    zypprepo { "obmondo_puppetlabs${version}":
+      ensure      => ensure_present($ensure),
+      name        => "obmondo_puppetlabs${version}",
+      baseurl     => "${source_protocol}://repos.obmondo.com/puppetlabs/sles/puppet${version}/${suseversion}/\$basearch/",
+      enabled     => 1,
+      autorefresh => 1,
+      gpgcheck    => 1,
+      type        => 'rpm-md',
+    }
   }
 
   eit_repos::yum::gpgkey { 'obmondo_puppetlabs':
-    ensure     => ensure_present($_ensure),
+    ensure     => ensure_present($ensure),
     path       => '/etc/pki/rpm-gpg/RPM-GPG-KEY-EnableIT_Aps_Ltd',
     source     => 'https://repos.obmondo.com/puppetlabs/public.key',
     noop_value => $noop_value,
