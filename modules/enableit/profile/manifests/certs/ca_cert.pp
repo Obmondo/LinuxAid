1657b430f8e1f1a9f66096ed42520a5114ea64a6
Ashish Jaiswal <ashish@obmondo.com>
2025-10-16 12:02:06 +0530

    feat: added content support for ca_cert and removed ensure, since that is not even used in the upstream define

--- modules/enableit/profile/manifests/certs/ca_cert.pp
+++ modules/enableit/profile/manifests/certs/ca_cert.pp
@@ -1,14 +1,14 @@
 # CA Cert
 define profile::certs::ca_cert (
-  Eit_types::Cert::Ensure $ensure,
-  Eit_Files::Source       $source,
-
-  Optional[Eit_types::Cert::Update] $update = undef,
+  Optional[Eit_Files::Source]       $source  = undef,
+  Optional[String]                  $content = undef,
+  Optional[Eit_types::Cert::Update] $update  = undef,
 ) {
 
   $_file = eit_files::to_file($source)
 
   trusted_ca::ca { $name:
-    source => $_file['resource']['source'],
+    source  => $_file['resource']['source'],
+    content => $_file['resource']['content'],
   }
 }
