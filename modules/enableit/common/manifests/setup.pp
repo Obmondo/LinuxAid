13ed58df6eff89045cb08491f913b4b70f771e99
Ashish Jaiswal <ashish@obmondo.com>
2025-11-19 12:28:02 +0530

    fix: create default directories for linuxaid, so we can create required pre-requisite, when setting up opensource way

--- modules/enableit/common/manifests/setup.pp
+++ modules/enableit/common/manifests/setup.pp
@@ -2,8 +2,20 @@
 #
 # @param noop_value Boolean value to control noop execution mode. Defaults to false.
 #
+# @param $__conf_dir
+# Absolute path to the configuration directory. Defaults to '/etc/obmondo'.
+#
+# @param $__opt_dir
+# Absolute path to the optional directory. Defaults to '/opt/obmondo'.
+#
+# @param $__bin_dir
+# Absolute path to the binary directory. Defaults to '/opt/obmondo/bin'.
+#
 class common::setup (
-  Boolean $noop_value = false,
+  Boolean              $noop_value = false,
+  Stdlib::Absolutepath $__conf_dir = '/etc/obmondo',
+  Stdlib::Absolutepath $__opt_dir  = '/opt/obmondo',
+  Stdlib::Absolutepath $__bin_dir  = '/opt/obmondo/bin',
 ) {
 
   # Create Obmondo group for exporter to run under this group
@@ -11,5 +23,21 @@ class common::setup (
     ensure => present,
     system => true,
     noop   => $noop_value,
+  }
+
+  file {
+    default:
+    ensure => ensure_dir($::obmondo_monitor),
+    noop   => $noop_value,
+      ;
+
+    [
+      $__conf_dir,
+      $__opt_dir,
+      "${__opt_dir}/home",
+      "${__opt_dir}/share",
+      "${__opt_dir}/etc",
+    ]:
+      ;
   }
 }
