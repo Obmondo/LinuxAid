5d7e46c245290a126f5db0904d2a29b7b0c1aca4
Ashish Jaiswal <ashish@obmondo.com>
2025-11-20 14:47:04 +0530

    feat: added cron feature to run on sysvinit node, which is turrisOS

--- modules/enableit/profile/manifests/openvox/run_openvox.pp
+++ modules/enableit/profile/manifests/openvox/run_openvox.pp
@@ -3,16 +3,19 @@ class profile::openvox::run_openvox (
   Eit_types::Noop_Value $noop_value = $common::openvox::noop_value,
 ) {
 
-  package { 'obmondo-run-puppet':
-    ensure => absent,
-    noop   => $noop_value,
-  }
-
-  file { '/etc/default/run_puppet':
-    ensure => absent,
+  if $facts['init_system'] == 'sysvinit' {
+    cron { 'run-openvox':
+      ensure  => present,
+      command => '/opt/obmondo/bin/linuxaid-cli run-openvox',
+      user    => 'root',
+      minute  => fqdn_rand(59, $facts['networking']['hostname']),
+      hour    => '*',
+      noop    => $noop_value,
+    }
   }
 
   if $facts['init_system'] == 'systemd' {
+    $_minutes = fqdn_rand(300, $facts['networking']['hostname'])
     Service {
       noop => $noop_value,
     }
@@ -21,8 +24,14 @@ class profile::openvox::run_openvox (
       noop => $noop_value,
     }
 
-    # Change the upgrade service timer timing.
-    $_minutes = fqdn_rand(300, $facts['networking']['hostname'])
+    package { 'obmondo-run-puppet':
+      ensure => absent,
+      noop   => $noop_value,
+    }
+
+    file { '/etc/default/run_puppet':
+      ensure => absent,
+    }
 
     $_timer = @("EOT"/$n)
       # THIS FILE IS MANAGED BY OBMONDO. CHANGES WILL BE LOST.
