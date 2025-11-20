a32a1c925b4e6e83b923111e7fc9565fb895063a
Ashish Jaiswal <ashish@obmondo.com>
2025-11-20 13:11:54 +0530

    fix: removed the dummy gem provider, and gave a default undef value for noop_value for repo define

--- modules/enableit/eit_repos/manifests/repo.pp
+++ modules/enableit/eit_repos/manifests/repo.pp
@@ -7,25 +7,26 @@ define eit_repos::repo (
       Array
     ]
   ]                     $versions   = undef,
-  Eit_types::Noop_Value $noop_value = $eit_repos::noop_value,
+  Eit_types::Noop_Value $noop_value = undef,
 ) {
 
-  $_package_provider = lookup('eit_repos::package_provider', String, undef, $facts['package_provider'])
   $os_name = $facts['os']['name']
-
-  $_options = if $versions != undef {
-    {
-      versions => $versions,
-    }
-  }
-
-  $_defaults = {
-    ensure     => $ensure,
-    noop_value => $noop_value,
-  }
 
   # Note: Skip repo setup for TurrisOS, for now.
   if $os_name != 'TurrisOS' {
+    $_package_provider = lookup('eit_repos::package_provider', String, undef, $facts['package_provider'])
+
+    $_options = if $versions != undef {
+      {
+        versions => $versions,
+      }
+    }
+
+    $_defaults = {
+      ensure     => $ensure,
+      noop_value => $noop_value,
+    }
+
     class { "eit_repos::${_package_provider}::${name}":
       * => stdlib::merge($_options, $_defaults),
     }
