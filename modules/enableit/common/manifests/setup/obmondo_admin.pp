510ad66b8f9f7d88386f9788f4939356bb66aa5b
Ashish Jaiswal <ashish@obmondo.com>
2025-11-20 09:59:45 +0530

    fix: setup obmondo_admin account only if it has subscription, for opensource User and TurrisOS we dont need one

--- modules/enableit/common/manifests/setup/obmondo_admin.pp
+++ modules/enableit/common/manifests/setup/obmondo_admin.pp
@@ -10,6 +10,7 @@
 #
 class common::setup::obmondo_admin (
   Boolean                 $manage          = true,
+  Boolean                 $ensure          = true,
   Optional[Array[String]] $manager_pubkeys = [],
   Optional[Array[String]] $sre_pubkeys     = [],
   Boolean                 $allow_sre       = true,
@@ -17,80 +18,83 @@ class common::setup::obmondo_admin (
 ) {
   contain common::setup
 
-  $_enable = $::obmondo_monitor and $manage
-  $__opt_dir = $common::setup::__opt_dir
-  $_home = "${__opt_dir}/home"
-  $_home_admin = "${_home}/obmondo-admin"
-  $_home_admin_ssh = "${_home_admin}/.ssh"
+  if $manage {
+    $_enable = $::obmondo_monitor and $ensure
+    $__opt_dir = $common::setup::__opt_dir
+    $_home = "${__opt_dir}/home"
+    $_home_admin = "${_home}/obmondo-admin"
+    $_home_admin_ssh = "${_home_admin}/.ssh"
 
-  file {
-    default:
-      ensure => stdlib::ensure($_enable, 'directory'),
-      noop   => $noop_value,
-    ;
+    file {
+      default:
+        ensure => stdlib::ensure($_enable, 'directory'),
+        noop   => $noop_value,
+      ;
 
-    $_home_admin:
-      owner   => 'obmondo-admin',
-      group   => 'obmondo',
-      mode    => '0755',
-      require => User['obmondo-admin'],
-    ;
+      $_home_admin:
+        owner   => 'obmondo-admin',
+        group   => 'obmondo',
+        mode    => '0755',
+        require => User['obmondo-admin'],
+      ;
 
-    $_home_admin_ssh:
-      ensure  => stdlib::ensure($_enable, 'directory'),
-      owner   => 'obmondo-admin',
-      group   => 'obmondo',
-      mode    => '0700',
-      require => File[$_home_admin],
-    ;
+      $_home_admin_ssh:
+        ensure  => stdlib::ensure($_enable, 'directory'),
+        owner   => 'obmondo-admin',
+        group   => 'obmondo',
+        mode    => '0700',
+        require => File[$_home_admin],
+      ;
 
-    [
-      '/etc/ssl/private/',
-      '/etc/ssl/private/letsencrypt',
-    ]:
-      mode  => '0700',
-      owner => 'root',
-      group => 'root',
-    ;
-  }
+      [
+        '/etc/ssl/private/',
+        '/etc/ssl/private/letsencrypt',
+      ]:
+        mode  => '0700',
+        owner => 'root',
+        group => 'root',
+      ;
+    }
 
-  user { 'obmondo-admin':
-    ensure         => stdlib::ensure($_enable),
-    comment        => 'Obmondo adminstration user',
-    forcelocal     => true,
-    gid            => 'obmondo',
-    system         => true,
-    shell          => '/bin/bash',
-    managehome     => true,
-    home           => '/opt/obmondo/home/obmondo-admin',
-    password       => '!',
-    purge_ssh_keys => true,
-    noop           => $noop_value,
-    require        => Group['obmondo'],
-  }
+    user { 'obmondo-admin':
+      ensure         => stdlib::ensure($_enable),
+      comment        => 'Obmondo adminstration user',
+      forcelocal     => true,
+      gid            => 'obmondo',
+      system         => true,
+      shell          => '/bin/bash',
+      managehome     => true,
+      home           => '/opt/obmondo/home/obmondo-admin',
+      password       => '!',
+      purge_ssh_keys => true,
+      noop           => $noop_value,
+      require        => Group['obmondo'],
+    }
 
-  profile::system::sudoers::conf { 'obmondo-admin':
-    content    => 'obmondo-admin ALL=(ALL) NOPASSWD: ALL',
-    noop_value => $noop_value,
-  }
+    profile::system::sudoers::conf { 'obmondo-admin':
+      ensure     => stdlib::ensure($_enable),
+      content    => 'obmondo-admin ALL=(ALL) NOPASSWD: ALL',
+      noop_value => $noop_value,
+    }
 
-  $all_pubkeys = ($allow_sre and $_enable) ? {
-    true    => $manager_pubkeys + $sre_pubkeys,
-    default => $manager_pubkeys,
-  }
+    $all_pubkeys = ($allow_sre and $_enable) ? {
+      true    => $manager_pubkeys + $sre_pubkeys,
+      default => $manager_pubkeys,
+    }
 
-  $all_pubkeys.each |$key| {
-    $_key = functions::split_ssh_key($key)
-    ssh_authorized_key { "obmondo-admin ssh key ${_key['comment']}":
-      ensure  => stdlib::ensure($_enable),
-      type    => $_key['type'],
-      key     => $_key['key'],
-      user    => 'obmondo-admin',
-      noop    => $noop_value,
-      require => [
-        User['obmondo-admin'],
-        File[$_home_admin, $_home_admin_ssh],
-      ],
+    $all_pubkeys.each |$key| {
+      $_key = functions::split_ssh_key($key)
+      ssh_authorized_key { "obmondo-admin ssh key ${_key['comment']}":
+        ensure  => stdlib::ensure($_enable),
+        type    => $_key['type'],
+        key     => $_key['key'],
+        user    => 'obmondo-admin',
+        noop    => $noop_value,
+        require => [
+          User['obmondo-admin'],
+          File[$_home_admin, $_home_admin_ssh],
+        ],
+      }
     }
   }
 }
