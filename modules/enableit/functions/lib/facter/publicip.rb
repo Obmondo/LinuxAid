f511b144cb592c34d8ca5c81a3bd10f09f7de5f7
iminfinity <iminfinity@pm.me>
2025-12-17 19:41:18 +0530

    test

--- modules/enableit/functions/lib/facter/publicip.rb
+++ modules/enableit/functions/lib/facter/publicip.rb
@@ -7,32 +7,38 @@ PORT = 443
 
 # NOTE: copied from here and tweaked a bit
 # https://gitlab.enableit.dk/obmondo/puppet/-/blob/master/modules/enableit/functions/lib/puppet/functions/api.rb
-def obmondo_api(endpoint)
-  raise NameError, "invalid endpoint '#{endpoint}'" unless
-    endpoint.is_a? String
+# def obmondo_api(endpoint)
+#   raise NameError, "invalid endpoint '#{endpoint}'" unless
+#     endpoint.is_a? String
 
-  proto = "http#{'s' if SSL}"
-  port = if PORT
-           ":#{PORT}"
-         end
-  uri = URI("#{proto}://api.obmondo.com#{port}/api#{endpoint}")
+#   proto = "http#{'s' if SSL}"
+#   port = if PORT
+#            ":#{PORT}"
+#          end
+#   uri = URI("#{proto}://api.obmondo.com#{port}/api#{endpoint}")
 
-  begin
-    Net::HTTP.start(uri.host, uri.port,
-                   use_ssl: SSL,
-                   open_timeout: 1, read_timeout: 2) do |http|
-      response = http.get(uri.path)
+#   begin
+#     Net::HTTP.start(uri.host, uri.port,
+#                    use_ssl: SSL,
+#                    open_timeout: 1, read_timeout: 2) do |http|
+#       response = http.get(uri.path)
 
-      return response.body if response.code.to_i == 200
+#       return response.body if response.code.to_i == 200
 
-      raise ArgumentError, "request failed: #{response}"
-    end
-  rescue SocketError => e
-    raise "API: unable to contact obmondo API #{e}"
-  rescue Timeout::Error => e
-    raise "API: Failed with timeout error #{e}"
-  end
-end
+#       raise ArgumentError, "request failed: #{response}"
+#     end
+#   rescue SocketError => e
+#     raise "API: unable to contact obmondo API #{e}"
+#   rescue Timeout::Error => e
+#     raise "API: Failed with timeout error #{e}"
+#   end
+# end
+
+BASEDIR = Dir.pwd == '/opt/puppetlabs/server/apps/puppetserver' ? '/etc/puppetlabs/code/environments/master' : Dir.pwd
+
+$LOAD_PATH.unshift("#{BASEDIR}/modules/enableit/functions/lib")
+
+require 'puppet/functions/api.rb'
 
 Facter.add('publicip') do
   setcode do
