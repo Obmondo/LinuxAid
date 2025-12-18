2a5f91afa834504b41f4890d85acc04e7897693f
Nitish Malang <nmalang@obmondo.com>
2025-12-18 13:34:58 +0530

    add ssl in cert

--- linuxaid_enc.rb
+++ linuxaid_enc.rb
@@ -5,7 +5,7 @@ require 'yaml'
 
 CERTNAME = ARGV[0]
 MAX_TAGS = 10
-DEFAULT_HIERA_BRANCH = 'enc'
+DEFAULT_HIERA_BRANCH = 'encnew'
 OBMONDO_MONITOR = true
 SUBSCRIPTION = false
 HIERA_DATADIR = '../../hiera-data'
