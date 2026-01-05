8f6200313ffd3494de197ffc76cfb2583fcab03e
aman <aman@obmondo.com>
2026-01-05 15:27:46 +0530

    remove repos

--- modules/enableit/eit_repos/manifests/apt/kubernetes.pp
+++ modules/enableit/eit_repos/manifests/apt/kubernetes.pp
@@ -13,7 +13,6 @@ class eit_repos::apt::kubernetes (
       architecture => 'amd64',
       # NOTE: empty string is on purpose, since all k8s package works for all distros
       release      => '',
-      repos        => '',
       key          => {
         'name'   => "kubernetes_${version}.gpg",
         'source' => 'puppet:///modules/eit_repos/apt/kubernetes-apt-keyring.gpg',
