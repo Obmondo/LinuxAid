07706470ff2486d0427d239aca463e5ebbe9c1ad
deepakt7340 <deepak@obmondo.com>
2026-01-09 12:32:39 +0530

    Enable slurm metrics

--- modules/enableit/profile/manifests/computing/slurm/slurmctld.pp
+++ modules/enableit/profile/manifests/computing/slurm/slurmctld.pp
@@ -1,18 +1,36 @@
 # Slurm slurm::slurmctld
 class profile::computing::slurm::slurmctld (
-  Eit_types::SimpleString $interface,
-  Array[Eit_types::IPCIDR] $node_cidrs,
+  Eit_types::SimpleString   $interface,
+  Array[Eit_types::IPCIDR]  $node_cidrs,
+  Stdlib::Port              $slurmctldport,
+  Eit_types::Slurm::Metrics $metrics,
+  Eit_types::Version        $slurm_version,
 ) {
+
+  $scrape_host = $::trusted['certname']
 
   firewall_multi {'100 allow slurmctld':
     jump    => 'accept',
     iniface => $interface,
     source  => $node_cidrs,
-    dport   => 6817,
+    dport   => $slurmctldport,
     proto   => 'tcp',
   }
 
   include ::slurm::slurmctld
   include common::monitor::exporter::slurm
-  include monitor::system::slurm
+
+  if versioncmp($slurm_version, '25.11') >= 0 {
+    $metrics.each |$metric| {
+      prometheus::scrape_job { "${metric}_${scrape_host}_${slurmctldport}":
+        job_name    => 'slurm',
+        collect_dir => '/etc/prometheus/file_sd_config.d',
+        targets     => [ "${scrape_host}:${slurmctldport}" ],
+        labels      => {
+          'certname'         => $scrape_host,
+          '__metrics_path__' => "/metrics/${metric}",
+        },
+      }
+    }
+  }
 }
