41b91c951258dc76cc558a67ba14575edd02c088
Ashish Jaiswal <ashish@obmondo.com>
2026-01-23 09:44:59 +0530

    fix: moved the mysql code from db class to db::mysql

--- modules/enableit/common/manifests/backup/db/mysql.pp
+++ modules/enableit/common/manifests/backup/db/mysql.pp
@@ -8,7 +8,7 @@
 #
 # @param ignore_tables List of tables to ignore during backup. Defaults to $::common::backup::db::ignore_tables.
 #
-# @param backup_method The backup method to use ('xtrabackup', 'mysqldump', 'mysqlbackup'). Defaults to $::common::backup::db::backup_method.
+# @param backup_method The backup method to use ('xtrabackup', 'mysqldump', 'mysqlbackup'). Defaults to $common::backup::db::backup_method.
 #
 # @param backup_user The user for backup operations. Defaults to $::common::backup::db::backup_user.
 #
@@ -27,19 +27,75 @@
 # @encrypt_params backup_user_password
 #
 class common::backup::db::mysql (
-  Eit_types::Password       $backup_user_password  = $::common::backup::db::backup_user_password,
-  Boolean                   $enable                = $::common::backup::db::enable,
-  Integer[0,23]             $backup_hour           = $::common::backup::db::backup_hour,
-  Array[String]             $ignore_tables         = $::common::backup::db::ignore_tables,
+  Eit_types::Password        $backup_user_password  = $::common::backup::db::backup_user_password,
+  Boolean                    $enable                = $::common::backup::db::enable,
+  Integer[0,23]              $backup_hour           = $::common::backup::db::backup_hour,
+  Array[String]              $ignore_tables         = $::common::backup::db::ignore_tables,
   Enum[
     'xtrabackup',
     'mysqldump',
     'mysqlbackup'
-  ]                         $backup_method         = $::common::backup::db::backup_method,
-  Eit_types::SimpleString   $backup_user           = $::common::backup::db::backup_user,
-  Stdlib::Absolutepath      $dump_dir              = $::common::backup::db::dump_dir,
-  Eit_types::Duration::Days $backup_retention      = $::common::backup::db::backup_retention,
-  Eit_types::Encrypt::Params $encrypt_params       = ['backup_user_password'],
+  ]                          $backup_method         = $::common::backup::db::backup_method,
+  Eit_types::SimpleString    $backup_user           = $::common::backup::db::backup_user,
+  Stdlib::Absolutepath       $dump_dir              = $::common::backup::db::dump_dir,
+  Eit_types::Duration::Days  $backup_retention      = $::common::backup::db::backup_retention,
+  Eit_types::Encrypt::Params $encrypt_params        = ['backup_user_password'],
+  Optional[Eit_types::Host]  $host                  = 'localhost',
+
+  Optional[Eit_types::Password] $root_password = $::common::backup::root_password,
+
 ) inherits ::common::backup::db {
   contain "common::backup::db::mysql::${backup_method}"
+
+  # Since the mysql module only support backup from localhost.
+  # For taking backup from different host we are using our own script.
+  if $host != 'localhost' {
+    file {'/opt/obmondo/bin/mysqlbackup.sh':
+      ensure  => ensure_present($enable),
+      mode    => '0700',
+      owner   => 'root',
+      group   => 'root',
+      content => epp('common/backup/mysqlbackup.sh.epp', {
+        backup_user          => $backup_user,
+        backup_user_password => $backup_user_password,
+        host                 => $host,
+        root_password        => $root_password,
+        backup_retention     => $backup_retention,
+        dump_dir             => $dump_dir,
+      }),
+    }
+    common::services::systemd { 'mysql-backup.timer':
+      ensure  => true,
+      enable  => true,
+      timer   => {
+        'OnCalendar' => systemd_make_timespec({
+          'year'   => '*',
+          'month'  => '*',
+          'day'    => '*',
+          'hour'   => $backup_hour,
+          'minute' => 0,
+          'second' => 0,
+        }),
+        'Unit'       => 'mysql-backup.service',
+      },
+      unit    => {
+        'Requires'  => 'mysql-backup.service',
+      },
+      install => {
+        'WantedBy' => 'timers.target',
+      },
+      require => File['/opt/obmondo/bin/mysqlbackup.sh'],
+    }
+    common::services::systemd { 'mysql-backup.service':
+      ensure  => 'present',
+      unit    => {
+        'Wants'    => 'mysql-backup.timer',
+      },
+      service => {
+        'Type'      => 'oneshot',
+        'ExecStart' => "/bin/sh -c '/opt/obmondo/bin/mysqlbackup.sh'",
+      },
+      require => File['/opt/obmondo/bin/mysqlbackup.sh'],
+    }
+  }
 }
