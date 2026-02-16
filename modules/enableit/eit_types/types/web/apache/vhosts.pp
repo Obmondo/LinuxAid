fc74c59d490bf9120ff8d20bc4f844a48f18347f
Ashish Jaiswal <ashish@obmondo.com>
2026-02-16 10:30:49 +0530

    fix: apache vhost can have any string, but the underlying domains needs to be fqdn, since its useful incase the entry is common in tags hiera

--- modules/enableit/eit_types/types/web/apache/vhosts.pp
+++ modules/enableit/eit_types/types/web/apache/vhosts.pp
@@ -1,2 +1,2 @@
 # Apache vhosts
-type Eit_types::Web::Apache::Vhosts = Hash[Stdlib::Fqdn, Eit_types::Web::Apache::Vhost_options]
+type Eit_types::Web::Apache::Vhosts = Hash[String, Eit_types::Web::Apache::Vhost_options]
