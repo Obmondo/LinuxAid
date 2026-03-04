1a9c08734de95dc089c24cd45e307d9ecff63cf0
aman <aman@obmondo.com>
2026-03-04 12:16:43 +0530

    Replace . with _ in environment

--- puppet_enc.rb
+++ puppet_enc.rb
@@ -62,6 +62,7 @@ server_data = if TESTING
 subscription_product_id = server_data&.dig('product_id')
 tag_keys = server_data&.dig('tags') || []
 linuxaid_tag = server_data&.dig('linuxaid_tag')
+linuxaid_tag = linuxaid_tag.gsub('.', '_')
 
 # ensure that we don't have too many tags -- if we allow for more tags, we also
 # need to update hiera.yaml!
