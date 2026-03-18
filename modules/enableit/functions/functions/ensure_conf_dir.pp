e391366fdeaa6a0219f5845028a3fb31f4016020
deepakt7340 <deepak@obmondo.com>
2026-03-18 14:41:49 +0530

    updating  the class name form common::setup::obmondo_admin to common::system::obmondo_admin

--- modules/enableit/functions/functions/ensure_conf_dir.pp
+++ modules/enableit/functions/functions/ensure_conf_dir.pp
@@ -2,7 +2,7 @@ function functions::ensure_conf_dir(
   Eit_types::SimpleString $name,
   Hash $options = {},
 ) {
-  ensure_resource('file', "${common::setup::__conf_dir}/${name}", {
+  ensure_resource('file', "${common::__conf_dir}/${name}", {
     ensure => directory,
   } + $options)
 }
