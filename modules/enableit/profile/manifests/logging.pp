7baa5674e808a8f692356ba3ea721813672e89f5
Gaurav <gaurav@obmondo.com>
2026-03-23 21:58:45 +0530

    add: group ensure for profile::logging

--- modules/enableit/profile/manifests/logging.pp
+++ modules/enableit/profile/manifests/logging.pp
@@ -6,12 +6,19 @@ class profile::logging (
 ) {
 
   if $manage {
+    # Ensure the configured logging group exists before it is used by file resources.
+    ensure_resource('group', $log_dir_group, {
+      ensure => present,
+      system => true,
+    })
+
     # On Debian/Ubuntu the rsyslog daemon runs as the `syslog` user/group, but
     # this user doesn't have write access to /var/log by default.
     file { '/var/log':
       ensure => directory,
       group  => $log_dir_group,
       mode   => $log_dir_mode,
+      require => Group[$log_dir_group],
     }
 
     # RHEL systems needs to have /var/log/rhsm, otherwise logs are output on
