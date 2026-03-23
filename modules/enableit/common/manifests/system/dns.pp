837dffd63f23945a0b4b47459a236a0d2aaa30ab
deepakt7340 <deepak@obmondo.com>
2026-03-23 20:29:32 +0530

    fix: Replaced profile::system::systemd with common::system::systemd so that systemd parameters are sourced from the common class

--- modules/enableit/common/manifests/system/dns.pp
+++ modules/enableit/common/manifests/system/dns.pp
@@ -50,7 +50,7 @@ class common::system::dns (
 
   if $manage {
     contain profile::system::dns
-    contain profile::system::systemd
+    contain common::system::systemd
   }
 
   if $facts['init_system'] == 'systemd' {
