cf996c3b302e2bae7b01c1b857b8fb94c4afad4b
aman <aman@obmondo.com>
2026-04-13 14:58:53 +0530

    Added the functionality to install conda forge packages

--- modules/enableit/common/manifests/software/miniforge.pp
+++ modules/enableit/common/manifests/software/miniforge.pp
@@ -8,15 +8,18 @@
 #
 # @param install_dir Stdlib::Absolutepath parameter to control installation directory of Miniforge3.
 #
+# @param conda_forge_packages Array of conda-forge package names to install (e.g. ['exec-wrappers']).
+#
 # @groups management manage, enable.
 #
-# @groups installation version, install_dir.
+# @groups installation version, install_dir, conda_forge_packages.
 #
 class common::software::miniforge (
-  Boolean              $manage      = false,
-  Boolean              $enable      = true,
-  Eit_types::Version   $version     = '25.9.1-0',
-  Stdlib::Absolutepath $install_dir = '/opt/miniforge',
+  Boolean              $manage               = false,
+  Boolean              $enable               = true,
+  Eit_types::Version   $version              = '25.9.1-0',
+  Stdlib::Absolutepath $install_dir          = '/opt/miniforge',
+  Array[String[1]]     $conda_forge_packages = [],
 ) {
 
   if $manage {
