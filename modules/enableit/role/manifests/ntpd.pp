3c018629c379c727502e38f509e980b9396e2a21
iminfinity <iminfinity@pm.me>
2026-08-06 19:58:03 +0530

    refactor: drop never-set role::ntpd params and inline their values

--- modules/enableit/role/manifests/ntpd.pp
+++ modules/enableit/role/manifests/ntpd.pp
@@ -3,31 +3,13 @@
 #
 # @param servers The list of NTPD servers. No default value.
 #
-# @param burst Whether to enable burst mode. Defaults to false.
-#
-# @param restrict The list of restrict configurations. Defaults to an empty array.
-#
-# @param tinker Whether to enable tinker options. Defaults to false.
-#
-# @param panic The panic threshold in seconds. Defaults to undef.
-#
-# @groups config burst, restrict, tinker, panic
-#
 # @groups network servers
 #
 class role::ntpd (
-  Array[Stdlib::Host]     $servers,
-  Optional[Boolean]       $burst    = false,
-  Optional[Array[String]] $restrict = [],
-  Optional[Boolean]       $tinker   = false,
-  Optional[Integer[0]]    $panic    = undef,
+  Array[Stdlib::Host] $servers,
 ) {
 
   class { '::profile::system::ntpd':
-    servers  => $servers,
-    burst    => $burst,
-    restrict => $restrict,
-    tinker   => $tinker,
-    panic    => $panic,
+    servers => $servers,
   }
 }
