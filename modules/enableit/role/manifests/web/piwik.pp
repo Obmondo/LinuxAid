3cdd7b5e17be76f33187d1ec2519123af013df53
iminfinity <iminfinity@pm.me>
2026-08-06 20:28:04 +0530

    refactor: drop 5 unconfigured piwik params and declare phpfpm and mysql directly

--- modules/enableit/role/manifests/web/piwik.pp
+++ modules/enableit/role/manifests/web/piwik.pp
@@ -5,23 +5,11 @@
 #
 # @param database_password The password for the database.
 #
-# @param variant The variant of PHP to use. Defaults to '::role::appeng::mod_php'.
-#
-# @param database The database type to use. Defaults to '::role::db::mysql'.
-#
-# @param force_https Whether to force HTTPS. Defaults to false.
-#
-# @param use_forwarded_for Whether to use the forwarded for header. Defaults to false.
-#
-# @param use_forwarded_host Whether to use the forwarded host header. Defaults to false.
-#
 # @param encrypt_params The list of params, which needs to be encrypted
 #
-# @groups network hostname, force_https, use_forwarded_for, use_forwarded_host
+# @groups network hostname
 #
-# @groups database database_password, database
-#
-# @groups php variant, encrypt_params
+# @groups database database_password, encrypt_params
 #
 # @encrypt_params database_password
 #
@@ -29,29 +17,22 @@ class role::web::piwik (
   String $hostname,
   Eit_types::Password $database_password,
 
-  Enum['::role::db::mysql'] $database           = '::role::db::mysql',
-  Boolean                   $force_https        = false,
-  Boolean                   $use_forwarded_for  = false,
-  Boolean                   $use_forwarded_host = false,
-
-  Enum['::role::appeng::mod_php', '::role::appeng::phpfpm'] $variant = '::role::appeng::mod_php',
-
   Eit_types::Encrypt::Params $encrypt_params = [
     'database_password',
   ],
 
 ) inherits ::role::web {
 
-  class { $variant: }
+  class { '::role::appeng::phpfpm': }
 
-  class { $database: }
+  class { '::role::db::mysql': }
 
   class { 'piwik':
     version            => 'latest',
     user               => 'www-data',
-    force_https        => $force_https,
-    use_forwarded_for  => $use_forwarded_for,
-    use_forwarded_host => $use_forwarded_host,
+    force_https        => false,
+    use_forwarded_for  => false,
+    use_forwarded_host => false,
   }
 
   apache::vhost { $hostname:
@@ -60,14 +41,10 @@ class role::web::piwik (
     require => Class['piwik'],
   }
 
-  if $database == '::role::db::mysql' {
-    mysql::db { 'piwik':
-      user     => 'piwik',
-      password => $database_password,
-      host     => 'localhost',
-      grant    => ['ALL'],
-    }
-  } else {
-    fail('We should probably do something here..')
+  mysql::db { 'piwik':
+    user     => 'piwik',
+    password => $database_password,
+    host     => 'localhost',
+    grant    => ['ALL'],
   }
 }
