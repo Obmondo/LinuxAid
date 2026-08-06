3a75906f6881ba18f6dc646276e66e36cf484f31
iminfinity <iminfinity@pm.me>
2026-08-06 20:34:17 +0530

    refactor: drop both redis role params, neither was ever configured

--- modules/enableit/role/manifests/db/redis.pp
+++ modules/enableit/role/manifests/db/redis.pp
@@ -1,19 +1,10 @@
 
 # @summary Class for managing the Redis database
 #
-# @param bind The IP address and port to bind to. Defaults to ['0.0.0.0'].
-#
-# @param default_port The default port for Redis. Defaults to 6379.
-#
-# @groups network bind, default_port.
-#
-class role::db::redis (
-  Array[Variant[Eit_types::IPPort, Eit_types::IP], 1] $bind            = ['0.0.0.0'],
-  Stdlib::Port $default_port                                         = 6379,
-) inherits ::role::db {
+class role::db::redis inherits ::role::db {
 
   class { 'profile::db::redis':
-    bind         => $bind,
-    default_port => $default_port,
+    bind         => ['0.0.0.0'],
+    default_port => 6379,
   }
 }
