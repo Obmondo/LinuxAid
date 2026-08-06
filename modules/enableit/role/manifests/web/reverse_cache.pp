0ef74faa34dc6c9621b114c34177a2bb790f9ec8
iminfinity <iminfinity@pm.me>
2026-08-06 20:23:02 +0530

    refactor: drop three never-set reverse_cache role params and inline their values

--- modules/enableit/role/manifests/web/reverse_cache.pp
+++ modules/enableit/role/manifests/web/reverse_cache.pp
@@ -1,8 +1,6 @@
 
 # @summary Reverse cache web role
 #
-# @param variant Defines which VCL to use.
-#
 # @param secret The secret for authentication.
 #
 # @param backendip The backend IP address.
@@ -11,37 +9,27 @@
 #
 # @param adminacl The admin ACL. Defaults to undef.
 #
-# @param adminlistens The admin listen addresses. Defaults to undef.
-#
-# @param purgers The purgers. Defaults to ['localhost'].
-#
 # @groups backend backendip, backendport.
 #
-# @groups admin adminacl, adminlistens.
+# @groups admin adminacl.
 #
 # @groups auth secret.
 #
-# @groups role variant, purgers.
-#
 class role::web::reverse_cache (
-  Enum['drupal'] $variant,
   Eit_types::UUID $secret,
   Eit_types::IP $backendip,
   Stdlib::Port $backendport,
   Optional[Array[String]] $adminacl = undef,
-  Optional[Array[Eit_types::AddressPort]] $adminlistens = undef,
-  Array[String] $purgers = ['localhost'],
 ) inherits role::web {
 
   # vcl, secret and other options should be arguments to THIS class
   # should also setup nginx - if ssl is selected
   class { '::profile::web::varnish':
-    backendip    => $backendip,
-    backendport  => $backendport,
-    adminacl     => $adminacl,
-    adminlistens => $adminlistens,
-    variant      => $variant,
-    purgers      => $purgers,
-    secret       => $secret,
+    backendip   => $backendip,
+    backendport => $backendport,
+    adminacl    => $adminacl,
+    variant     => 'drupal',
+    purgers     => ['localhost'],
+    secret      => $secret,
   }
 }
