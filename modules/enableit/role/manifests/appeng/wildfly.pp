621f7beb90db69a9398330edec8091a2cfb7a612
iminfinity <iminfinity@pm.me>
2026-08-06 20:39:37 +0530

    refactor: drop both unconfigured wildfly role params

--- modules/enableit/role/manifests/appeng/wildfly.pp
+++ modules/enableit/role/manifests/appeng/wildfly.pp
@@ -1,19 +1,10 @@
 
 # @summary Class for managing the Appeng Wildfly role
 #
-# @param version The version of Wildfly to use. Defaults to '8.2.0'.
-#
-# @param http_server The HTTP server to use. Defaults to 'apache'.
-#
-# @groups server http_server, version
-#
-class role::appeng::wildfly (
-  Enum['8.2.0', '9.0.0'] $version     = '8.2.0',
-  Enum['apache'] $http_server         = 'apache',
-) inherits role::web::java {
+class role::appeng::wildfly () inherits role::web::java {
 
   class { '::profile::appeng::wildfly':
-    version     => $version,
-    http_server => $http_server,
+    version     => '8.2.0',
+    http_server => 'apache',
   }
 }
