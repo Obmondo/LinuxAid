d294943428c481f1a7ea86bacb90132e5b9617c0
iminfinity <iminfinity@pm.me>
2026-08-07 15:18:31 +0530

    refactor: drop three never-set sssd params and read manage_sssd directly

--- modules/enableit/common/manifests/user_management/authentication/sssd.pp
+++ modules/enableit/common/manifests/user_management/authentication/sssd.pp
@@ -6,17 +6,11 @@
 #
 # @param enable Boolean to enable or disable SSSD. Defaults to false.
 #
-# @param manage Boolean to determine if the class should manage SSSD. Defaults to value of $common::user_management::authentication::manage_sssd.
-#
 # @param domains SSSD domain configurations. Defaults to empty hash.
 #
 # @param default_domain_suffix Default domain suffix. Defaults to undef.
 #
-# @param debug_level Debug level for SSSD. Defaults to '0x0620'.
-#
 # @param manage_oddjobd Boolean to manage oddjobd service. Defaults to true.
-#
-# @param full_name_format Format for full name. Defaults to '%1$s'.
 #
 # @param override_homedir Override for home directory. Defaults to undef.
 #
@@ -35,18 +29,15 @@ class common::user_management::authentication::sssd (
   ],
   Array[Eit_types::Sssd::Service]             $_available_services   = undef,
   Boolean                                     $enable                = false,
-  Boolean                                     $manage                = $common::user_management::authentication::manage_sssd,
   Eit_types::Sssd::Domains                    $domains               = {},
   Optional[Eit_types::Domain]                 $default_domain_suffix = undef,
-  Optional[Eit_types::Sssd::Debug_Level]      $debug_level           = '0x0620',
   Boolean                                     $manage_oddjobd        = true,
-  Eit_types::Sssd::Full_name_format           $full_name_format      = '%1$s',
   Optional[Eit_types::Sssd::Override_homedir] $override_homedir      = undef,
   Hash                                        $override_config       = {},
   Eit_types::Noop_Value                       $noop_value            = undef,
   Array                                       $required_packages     = ['realmd', 'samba-common-tools', 'fprintd-pam'],
 ) {
-  if $manage {
+  if $common::user_management::authentication::manage_sssd {
     include ::profile::system::authentication::sssd
   }
 }
