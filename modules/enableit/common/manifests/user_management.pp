098adc40c5e502db08795785ee31f2a6ebbf6b40
iminfinity <iminfinity@pm.me>
2026-08-07 15:13:43 +0530

    refactor: drop the four never-set common::user_management subsystem toggles

--- modules/enableit/common/manifests/user_management.pp
+++ modules/enableit/common/manifests/user_management.pp
@@ -1,33 +1,10 @@
 # @summary Class for managing user related configurations
 #
-# @param authentication Boolean to enable authentication management. Defaults to true.
-#
-# @param security Boolean to enable security management. Defaults to true.
-#
-# @param motd Boolean to enable MOTD management. Defaults to true.
-#
-# @param sshd Boolean to enable SSHD management. Defaults to true.
-#
-# @groups management authentication, security, motd, sshd
-#
-class common::user_management (
-  Boolean $authentication = true,
-  Boolean $security       = true,
-  Boolean $motd           = true,
-  Boolean $sshd           = true,
-) {
-  if $authentication {
-    contain common::user_management::authentication
-  }
-  if $security {
-    contain common::user_management::security
-  }
-  if $motd {
-    contain common::user_management::motd
-  }
-  if $sshd {
-    contain common::user_management::sshd
-  }
+class common::user_management {
+  contain common::user_management::authentication
+  contain common::user_management::security
+  contain common::user_management::motd
+  contain common::user_management::sshd
 
   unless lookup('common::user_management::jumphost::configs', Hash, undef, {}).empty {
     contain common::user_management::jumphosts
