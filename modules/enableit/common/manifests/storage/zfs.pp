01f3f2c2f478fa78355bfbbf4f16f6dd6cdccb0b
iminfinity <iminfinity@pm.me>
2026-08-07 15:21:49 +0530

    refactor: drop three never-set common::storage::zfs params and inline their values

--- modules/enableit/common/manifests/storage/zfs.pp
+++ modules/enableit/common/manifests/storage/zfs.pp
@@ -2,31 +2,21 @@
 #
 # @param enable Boolean indicating if ZFS should be enabled. Defaults to false.
 #
-# @param remove_sysstat_cron Boolean to remove sysstat cron jobs. Defaults to true.
-#
-# @param scrub Interval for ZFS scrubbing. Defaults to 'monthly'.
-#
 # @param allow_sync_from List of authorized sync sources.
 #
 # @param pools ZFS storage pool configurations.
 #
-# @param templates Templates for pool setup.
-#
 # @param replications Configurations for data replications.
 #
-# @groups general enable, remove_sysstat_cron, scrub
+# @groups general enable
 #
-# @groups replication allow_sync_from, pools, templates, replications
+# @groups replication allow_sync_from, pools, replications
 #
 class common::storage::zfs (
-  Boolean                       $enable              = false,
-  Boolean                       $remove_sysstat_cron = true,
-  Array[String]                 $allow_sync_from     = [],
-  Sanoid::Pools                 $pools               = {},
-  Optional[Sanoid::Templates]   $templates           = undef,
-  Sanoid::Syncoid::Replications $replications        = {},
-
-  Eit_types::Common::Storage::Zfs::Scrub_interval $scrub = 'monthly',
+  Boolean                       $enable          = false,
+  Array[String]                 $allow_sync_from = [],
+  Sanoid::Pools                 $pools           = {},
+  Sanoid::Syncoid::Replications $replications    = {},
 ) inherits common::storage {
 
   if $enable {
