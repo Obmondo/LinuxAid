999db70385b2630d9684601780ba684e5b805c09
iminfinity <iminfinity@pm.me>
2026-08-07 15:19:14 +0530

    refactor: drop the never-set sudo ssh_agent_auth param and inline its value

--- modules/enableit/common/manifests/user_management/authentication/sudo.pp
+++ modules/enableit/common/manifests/user_management/authentication/sudo.pp
@@ -4,15 +4,12 @@
 #
 # @param purge Whether to purge existing sudo configurations. Defaults to false.
 #
-# @param ssh_agent_auth Whether to enable SSH agent authentication. Defaults to false.
-#
 # @param sudoers The sudoers hash configuration. Defaults to an empty hash.
 #
 # @param sudoers_d_dir The absolute path to the sudoers.d directory. Defaults to '/etc/obmondo/sudoers.d'.
 class common::user_management::authentication::sudo (
   Boolean              $manage         = $common::user_management::authentication::manage_sudo,
   Boolean              $purge          = false,
-  Boolean              $ssh_agent_auth = false,
   Eit_types::Sudoers   $sudoers        = {},
   Stdlib::Absolutepath $sudoers_d_dir  = '/etc/obmondo/sudoers.d',
 ) {
