661020dde3dfccaadff69a2920be4d8487303918
iminfinity <iminfinity@pm.me>
2026-08-07 15:17:24 +0530

    refactor: drop the never-set dns fallback_nameservers and allow_external params

--- modules/enableit/common/manifests/system/dns.pp
+++ modules/enableit/common/manifests/system/dns.pp
@@ -6,8 +6,6 @@
 #
 # @param nameservers List of DNS nameservers. Defaults to an empty array.
 #
-# @param fallback_nameservers List of fallback DNS nameservers. Defaults to an empty array.
-#
 # @param searchpath DNS search path. Defaults to an empty array.
 #
 # @param dnssec Enable DNSSEC validation. Can be Boolean or 'allow-downgrade'. Defaults to false.
@@ -16,13 +14,11 @@
 #
 # @param listen_address List of IP addresses to listen on. Defaults to an empty array.
 #
-# @param allow_external Allow external DNS queries. Defaults to false.
-#
 # @param noop_value Value used for noop operations. Defaults to undef.
 #
-# @groups configuration resolver, manage, nameservers, fallback_nameservers, searchpath.
+# @groups configuration resolver, manage, nameservers, searchpath.
 #
-# @groups security dnssec, dns_over_tls, allow_external.
+# @groups security dnssec, dns_over_tls.
 #
 # @groups network listen_address.
 #
@@ -32,12 +28,10 @@ class common::system::dns (
   Enum['dnsmasq', 'systemd-resolved', 'resolv'] $resolver,
   Boolean                                       $manage               = false,
   Array                                         $nameservers          = [],
-  Array                                         $fallback_nameservers = [],
   Array[Eit_types::Hostname]                    $searchpath           = [],
   Variant[Boolean, Enum['allow-downgrade']]     $dnssec               = false,
   Variant[Boolean, Enum['opportunistic']]       $dns_over_tls         = false,
   Array[Stdlib::IP::Address]                    $listen_address       = [],
-  Boolean                                       $allow_external       = false,
   Eit_types::Noop_Value                         $noop_value           = undef,
 ) inherits common::system {
 
@@ -45,9 +39,6 @@ class common::system::dns (
           '`$nameservers` must have one or more values.')
   confine($manage, $resolver != 'systemd-resolved', $dnssec or $dns_over_tls,
           "`\$dnssec` and `\$dns_over_tls` are not available for resolver '${resolver}'")
-  confine($manage, $allow_external, $resolver != 'dnsmasq',
-          'Only `dnsmasq` can be used with `$allow_external`')
-
   if $manage {
     contain profile::system::dns
   }
