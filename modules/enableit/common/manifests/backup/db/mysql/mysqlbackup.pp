bb4b3afb296dd86dfc8a673d93eacd5ffef1182e
iminfinity <iminfinity@pm.me>
2026-08-07 15:22:35 +0530

    refactor: drop 6 never-configured common::backup::db::mysql::mysqlbackup params and read them from common::backup::db

--- modules/enableit/common/manifests/backup/db/mysql/mysqlbackup.pp
+++ modules/enableit/common/manifests/backup/db/mysql/mysqlbackup.pp
@@ -1,61 +1,35 @@
 # @summary Class for managing MySQL backup configuration and override for common::backup::db settings
 #
-# @param enable Whether to enable the MySQL backup. Defaults to the value of $::common::backup::db::enable.
-#
 # @param backup_user_password The password for the backup user. Defaults to $::common::backup::db::backup_user_password.
-#
-# @param backup_hour The hour of backup. Defaults to $::common::backup::db::backup_hour.
-#
-# @param ignore_tables List of tables to ignore during backup. Defaults to $::common::backup::db::ignore_tables.
-#
-# @param backup_user The user to perform backup with. Defaults to $::common::backup::db::backup_user.
-#
-# @param dump_dir Directory for dump files. Defaults to $::common::backup::db::dump_dir.
-#
-# @param backup_retention Number of days to retain backups. Defaults to $::common::backup::db::backup_retention.
 #
 # @param encrypt_params The list of params, which needs to be encrypted
 #
 # @encrypt_params backup_user_password
 #
-# @groups general enable, encrypt_params
-#
-# @groups authentication backup_user, backup_user_password
-#
-# @groups schedule backup_hour
-#
-# @groups storage dump_dir
-#
-# @groups retention backup_retention, ignore_tables
+# @groups authentication backup_user_password, encrypt_params
 #
 class common::backup::db::mysql::mysqlbackup (
-  Boolean                   $enable                = $::common::backup::db::enable,
-  Eit_types::Password       $backup_user_password  = $::common::backup::db::backup_user_password,
-  Integer[0,23]             $backup_hour           = $::common::backup::db::backup_hour,
-  Array[String]             $ignore_tables         = $::common::backup::db::ignore_tables,
-  Eit_types::User           $backup_user           = $::common::backup::db::backup_user,
-  Stdlib::Absolutepath      $dump_dir              = $::common::backup::db::dump_dir,
-  Eit_types::Duration::Days $backup_retention      = $::common::backup::db::backup_retention,
+  Eit_types::Password        $backup_user_password = $::common::backup::db::backup_user_password,
   Eit_types::Encrypt::Params $encrypt_params       = ['backup_user_password'],
 ) inherits ::common::backup::db {
 
-  $_ignore_tables = $ignore_tables.map |$table| {
+  $_ignore_tables = $::common::backup::db::ignore_tables.map |$table| {
     # https://dev.mysql.com/doc/refman/8.0/en/mysqldump.html#option_mysqldump_ignore-table
     "--ignore-table=${table}"
   }.delete_undef_values
 
   class { 'mysql::server::backup' :
     provider           => 'mysqlbackup',
-    backupuser         => $backup_user,
+    backupuser         => $::common::backup::db::backup_user,
     backuppassword     => $backup_user_password,
-    backupdir          => $dump_dir,
+    backupdir          => $::common::backup::db::dump_dir,
     backupcompress     => true,
-    backuprotate       => $backup_retention,
+    backuprotate       => $::common::backup::db::backup_retention,
     backupmethod       => 'mysqldump',
     delete_before_dump => false,
     include_routines   => true,
     include_triggers   => true,
-    time               => [$backup_hour, 3], # (i.e., 03:45) for HH:MM
+    time               => [$::common::backup::db::backup_hour, 3], # (i.e., 03:45) for HH:MM
     optional_args      => $_ignore_tables,
     install_cron       => false,
   }
