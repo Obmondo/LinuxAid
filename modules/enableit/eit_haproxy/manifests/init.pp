28af9964b04c624749065c11fa8149eb73fcbd51
Sidharth Jawale <sidharth@obmondo.com>
2026-09-07 20:44:25 +0530

    fix: allow apt to be safely referenced without duplicate declaration errors
    Signed-off-by: Sidharth Jawale <sidharth@obmondo.com>

--- modules/enableit/eit_haproxy/manifests/init.pp
+++ modules/enableit/eit_haproxy/manifests/init.pp
@@ -125,7 +125,7 @@ class eit_haproxy (
         # Newer Ubuntu LTS (26.04+) ships HAProxy 3.x in stock repos,
         # so the PPA is unnecessary there.
         if $facts['os']['release']['major'] =~ /^24/ {
-          contain apt
+          include apt
 
           $haproxy_lts_version = '3.2'
           apt::ppa { "ppa:vbernat/haproxy-${haproxy_lts_version}": }
