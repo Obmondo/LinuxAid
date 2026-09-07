4c66263f899a7c7cd593f27e72c5ce588349a63b
Sidharth Jawale <sidharth@obmondo.com>
2026-09-07 21:08:53 +0530

    fix: remove apt class declaration to fix eit_repos and eit_haproxy ordering issues
    Signed-off-by: Sidharth Jawale <sidharth@obmondo.com>

--- modules/enableit/eit_haproxy/manifests/init.pp
+++ modules/enableit/eit_haproxy/manifests/init.pp
@@ -125,10 +125,6 @@ class eit_haproxy (
         # Newer Ubuntu LTS (26.04+) ships HAProxy 3.x in stock repos,
         # so the PPA is unnecessary there.
         if $facts['os']['release']['major'] =~ /^24/ {
-          if ! defined(Class['apt']) {
-            class { 'apt': }
-          }
-
           $haproxy_lts_version = '3.2'
           apt::ppa { "ppa:vbernat/haproxy-${haproxy_lts_version}": }
 
