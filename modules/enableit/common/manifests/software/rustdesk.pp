d0e27cf24e0635c0e3f1eabe66d4817615f40c57
Sidharth Jawale <sidharth@obmondo.com>
2026-09-08 15:37:09 +0530

    fix: remove expose param for rustdesk since it's expected to setup it alongside haproxy role
    Signed-off-by: Sidharth Jawale <sidharth@obmondo.com>

--- modules/enableit/common/manifests/software/rustdesk.pp
+++ modules/enableit/common/manifests/software/rustdesk.pp
@@ -14,15 +14,13 @@
 #
 # @param server_extra_dependencies Array[String] parameter to install OS specific dependencies. Defaults to [].
 #
-# @param expose Whether to expose the service via HAProxy. Defaults to false.
-#
 # @groups management manage.
 #
 # @groups client client_enable, client_version, client_extra_dependencies.
 #
 # @groups server server_enable, server_version, server_extra_dependencies.
 #
-# @groups networking expose, domains.
+# @groups networking, domains.
 class common::software::rustdesk (
   Boolean              $manage                    = false,
 
@@ -33,13 +31,8 @@ class common::software::rustdesk (
   Boolean              $server_enable             = false,
   Array[String]        $server_extra_dependencies = [],
   Eit_types::Version   $server_version            = '1.7.1',
-
-  Boolean              $expose                    = false,
 ) {
   if $manage {
     include profile::software::rustdesk
-    if $expose {
-      include profile::web::haproxy
-    }
   }
 }
