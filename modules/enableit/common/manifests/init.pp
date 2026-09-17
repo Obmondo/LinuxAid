32ed7e63d44237bdfc5b2966ec0cde3609b7a8bb
Sidharth Jawale <sidharth@obmondo.com>
2026-09-17 21:09:40 +0530

    fix: prevent Prometheus/Node Exporter service errors on TurrisOS
    Signed-off-by: Sidharth Jawale <sidharth@obmondo.com>

--- modules/enableit/common/manifests/init.pp
+++ modules/enableit/common/manifests/init.pp
@@ -18,7 +18,7 @@ class common (
   Exec { path => ['/bin', '/usr/bin', '/usr/sbin', '/usr/local/bin'] }
   Stage['setup'] -> Stage['main']
 
-  if $::obmondo_monitoring_status { #lint:ignore:top_scope_facts
+  if $::obmondo_monitoring_status and $facts['os']['name'] != 'TurrisOS' { #lint:ignore:top_scope_facts
     # NOTE: Lets not allow anyone to remove our public repo, otherwise monitoring won't be setup
     # NOTE: For now, ignore setting up monitoring for TurrisOS, since opkg isn't supported as package provider.
     # NOTE: Later this needs to be fixed.
