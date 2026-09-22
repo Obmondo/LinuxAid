7bb695f4610165de5e6390ca67bd01485557a948
Sidharth Jawale <sidharth@obmondo.com>
2026-09-22 06:11:37 +0530

    fix: remove version handling for rustdesk client and server
    Signed-off-by: Sidharth Jawale <sidharth@obmondo.com>

--- modules/enableit/common/manifests/software/rustdesk.pp
+++ modules/enableit/common/manifests/software/rustdesk.pp
@@ -4,21 +4,17 @@
 #
 # @param client_enable Boolean parameter to enable or disable the Rustdesk client. Defaults to false.
 #
-# @param client_version SemVer parameter to control Rustdesk client version. Defaults to 1.4.3.
-#
 # @param client_extra_dependencies Array[String] parameter to install OS specific dependencies. Defaults to [].
 #
 # @param server_enable Boolean parameter to enable or disable the Rustdesk server. Defaults to false.
-#
-# @param server_version SemVer parameter to control Rustdesk server version. Default to 1.7.1.
 #
 # @param server_extra_dependencies Array[String] parameter to install OS specific dependencies. Defaults to [].
 #
 # @groups management manage.
 #
-# @groups client client_enable, client_version, client_extra_dependencies.
+# @groups client client_enable, client_extra_dependencies.
 #
-# @groups server server_enable, server_version, server_extra_dependencies.
+# @groups server server_enable, server_extra_dependencies.
 #
 # @groups networking, domains.
 class common::software::rustdesk (
@@ -26,11 +22,9 @@ class common::software::rustdesk (
 
   Boolean              $client_enable             = false,
   Array[String]        $client_extra_dependencies = [],
-  Eit_types::Version   $client_version            = '1.4.3',
 
   Boolean              $server_enable             = false,
   Array[String]        $server_extra_dependencies = [],
-  Eit_types::Version   $server_version            = '1.7.1',
 ) {
   if $manage {
     include profile::software::rustdesk
