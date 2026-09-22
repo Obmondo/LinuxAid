66a91bc5e42ddfa1a12019345bb0fa420ab41dc9
Sidharth Jawale <sidharth@obmondo.com>
2026-09-22 06:11:14 +0530

    fix: remove version handling for rustfs
    Signed-off-by: Sidharth Jawale <sidharth@obmondo.com>

--- modules/enableit/profile/manifests/storage/rustfs.pp
+++ modules/enableit/profile/manifests/storage/rustfs.pp
@@ -1,7 +1,6 @@
 # @summary RustFS storage profile
 #
 # @param data_dir The host directory where backup data is stored.
-# @param version The version of rustfs container image to use.
 # @param access_key The S3 access key ID.
 # @param secret_key The S3 secret access key.
 # @param env_vars Additional hash of environment variables for the container.
@@ -12,7 +11,6 @@ class profile::storage::rustfs (
   String[1]            $secret_key    = $role::storage::rustfs::secret_key,
   Boolean              $enable        = $role::storage::rustfs::enable,
   Stdlib::Unixpath     $data_dir      = $role::storage::rustfs::data_dir,
-  String[1]            $version       = '1.0.0',
   Hash                 $env_vars      = {},
 ) {
   # 1. Manage RustFS
@@ -21,7 +19,6 @@ class profile::storage::rustfs (
     secret_key => $secret_key,
     enable     => $enable,
     data_dir   => $data_dir,
-    version    => $version,
     env_vars   => $env_vars,
   }
 }
